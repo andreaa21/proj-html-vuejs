29427a01c2f8a3ba8da7451b2bc03f0f871d6d29
andreaa21 <andrea.zollini@proton.me>
2022-11-24 15:55:28 +0100

    [add] MainFeedback markup

--- src/components/Mainfeedback.vue
+++ src/components/Mainfeedback.vue
@@ -6,8 +6,40 @@ export default {
 
 
 <template>
-  <div class="debug">
-    Feedback
+  <div class="feedback debug">
+    <div class="container debug h-100">
+        <div class="row debug d-flex h-50 align-items-end text-center p-4">
+            <div class="col">
+                <div class="text-area">
+                    <h4>
+                        titolo piccolo
+                    </h4>
+                    <h2>
+                        titolo grande
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere rem error sequi! Quae, officia voluptate.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+        <div class="row h-50 d-flex align-items-center debug">
+            <div class="col">
+                <div class="card-area debug d-flex justify-content-between">
+                    <div class="info-card p-3">
+                        1
+                    </div>
+                    <div class="info-card p-3">
+                        2
+                    </div>
+                    <div class="info-card p-3">
+                        3
+                    </div>
+            </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +47,10 @@ export default {
 
 <style lang="scss" scoped>
 
+@use '../assets/styles/partials/vars' as *;
+
+    .feedback{
+        height: 775px;
+        background-color: $Nurse;
+    }
 </style>
