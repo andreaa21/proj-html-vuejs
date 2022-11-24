1678d07ef8b3f0a25d81e13bf1bca0d00de84962
andreaa21 <andrea.zollini@proton.me>
2022-11-24 18:14:57 +0100

    add style to AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,14 @@
 <script>
+
+import {store} from '../assets/data/store'
+
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  data(){
+    return{
+        store
+    }
+  }
 }
 </script>
 
@@ -8,18 +16,43 @@ export default {
 <template>
   <header>
     <div class="container h-100">
-        <div class="row debug header-content d-flex justify-content-between">
-            <div class="col header-left debug">
-                hours
-            </div>
-            <div class="col-lg-6 header-right d-flex justify-content-between debug">
-                <div class="contacts debug w-50">
-                    contacts
+        <div class="row header-content h-100 d-flex justify-content-between">
+
+            <div class="col header-left d-flex align-items-center">
+                <div class="time-icon me-3">
+                    <i class="fa-solid fa-clock"></i>
                 </div>
-                <div class="socials debug w-50">
-                    socials
+                <div class="time-text d-flex align-items-end">
+                    <span>Open Hours: {{store.openHours}}</span>
                 </div>
             </div>
+
+            <div class="col-5 header-right d-flex justify-content-between">
+                <div class="contacts d-flex align-items-center">
+                    <div class="phone mx-2">
+                        <i class="fa-solid fa-phone"></i>
+                        <span class="mx-2">{{store.phoneNumber}}</span>
+                    </div>
+                    <div class="email mx-2">
+                        <i class="fa-solid fa-envelope"></i>
+                        <span class="mx-2">{{store.email}}</span>
+                    </div>
+                </div>
+                <div class="socials">
+                    <ul class="d-flex justify-content-around h-100 align-items-center">
+                        <li>
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     </div>
   </header>
@@ -33,8 +66,18 @@ export default {
         height: 50px;
         background-color: $Ebony;
         color: $Nurse;
-        .header-content{
-            height: 100%;
+        .contacts{
+            width: 70%;
+        }
+        .socials{
+            width: 30%;
+        }
+        ul{
+            list-style: none;
+            margin-left: -1.5rem;
+            a{
+                color: $Nurse;
+            }
         }
     }
 </style>
