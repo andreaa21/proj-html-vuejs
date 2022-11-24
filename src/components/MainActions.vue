240aa4d6e875c8836b70d0249654d3ccc711ba95
andreaa21 <andrea.zollini@proton.me>
2022-11-24 15:05:59 +0100

    [add] MainActions markup

--- src/components/MainActions.vue
+++ src/components/MainActions.vue
@@ -6,13 +6,38 @@ export default {
 
 
 <template>
-    <div class="debug">
-        Actions
+    <div class="actions">
+        <div class="container h-100 debug">
+            <div class="row d-flex h-100 align-items-center">
+                <div class="col debug">
+                    <div class="title-area debug text-center">
+                        <h4>
+                            titolo piccolo
+                        </h4>
+                        <h2>
+                            titolo grande
+                        </h2>
+                    </div>
+                    <div class="card-wrapper debug text-center">
+                        <nav>
+                            nav
+                        </nav>
+                        card
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+    @use '../assets/styles/partials/vars' as *;
 
+    .actions{
+        height: 1310px;
+        background-color: $Woodsmoke;
+        color: $White;
+    }
 </style>
