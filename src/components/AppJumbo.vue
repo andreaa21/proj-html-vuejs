042d2f66fe6a87f7f8eb64a899ad9a498c419e87
andreaa21 <andrea.zollini@proton.me>
2022-11-24 18:24:55 +0100

    [add] inner element to AppJumbo

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -7,38 +7,40 @@ export default {
 
 <template>
   <div class="jumbotron">
-    <div class="container">
-        <div class="row debug d-flex justify-content-between align-items-center jumbo-top">
-            <div class="col">
-                <div class="logo debug">
-                    Logo
+    <div class="inner">
+        <div class="container">
+            <div class="row debug d-flex justify-content-between align-items-center jumbo-top">
+                <div class="col">
+                    <div class="logo debug">
+                        Logo
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="jumbo-options d-flex debug">
+                        <nav>
+                            nav
+                        </nav>
+                        <button>
+                            get in touch
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="jumbo-options d-flex debug">
-                    <nav>
-                        nav
-                    </nav>
-                    <button>
-                        get in touch
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="row debug jumbo-bottom d-flex align-items-center">
-            <div class="jumbo-content debug">
-                <h3>titolo piccolo</h3>
-                <h2>titolo grande</h2>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quidem architecto, pariatur omnis facere fuga debitis vel modi suscipit dolore. Obcaecati esse inventore molestiae porro magni repellat repudiandae nihil voluptatum!
-                </p>
-                <div class="btn-area">
-                    <button>
-                        get in touch
-                    </button>
-                    <button>
-                        read more
-                    </button>
+            <div class="row debug jumbo-bottom d-flex align-items-center">
+                <div class="jumbo-content debug">
+                    <h3>titolo piccolo</h3>
+                    <h2>titolo grande</h2>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quidem architecto, pariatur omnis facere fuga debitis vel modi suscipit dolore. Obcaecati esse inventore molestiae porro magni repellat repudiandae nihil voluptatum!
+                    </p>
+                    <div class="btn-area">
+                        <button>
+                            get in touch
+                        </button>
+                        <button>
+                            read more
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -54,6 +56,9 @@ export default {
         min-height: 860px;
         background-image: url('../assets/img/bg-4.jpg');
         color: $White;
+        .inner{
+            background-color: rgba(0, 0, 0, 0.356);
+        }
         .jumbo-top{
             min-height: 100px;
         }
