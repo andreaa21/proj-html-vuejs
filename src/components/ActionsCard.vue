1276f51be4f439feaef513283ec45800e184b2bd
andreaa21 <andrea.zollini@proton.me>
2022-11-25 18:45:11 +0100

    [change] style to ActionCards

--- src/components/ActionsCard.vue
+++ src/components/ActionsCard.vue
@@ -23,7 +23,7 @@ export default {
     :key="index"
     :style="`background-image: url(/${card.action_bg})`"
     class="col-4 d-flex flex-column justify-content-end action-card">
-      <div class="card-text text-center">
+      <div class="card-text inner w-100 h-100 text-center d-flex align-items-end">
         <h2>
           {{card.action_text}}
         </h2>
@@ -35,21 +35,28 @@ export default {
 
 
 <style lang="scss" scoped>
-
+  @use '../assets/styles/partials/vars' as *;
   .inner{
-    background-color: rgba(0, 0, 0, 0.418);
+    background-color: rgba(0, 0, 0, 0.473);
+    position:absolute;
+    border-radius: 10px;
+    left: 0;
+    top: 0;
   }
   .action-card{
     width: 360px;
     height: 360px;
     background-size: cover;
-    border-radius: 20px;
+    border-radius: 10px;
     margin: .8rem;
+    position: relative;
     .card-text{
       margin-bottom: 2.5rem;
       h2{
         font-size: 1.5rem;
         font-weight: 900;
+        padding: 2rem .5rem;
+        color: $Nurse;
       }
     }
   }
